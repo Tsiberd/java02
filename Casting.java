e15a3d85ec716a941f6ace00942536045fd6091b
kostas Tsimp <tsiberd@yahoo.gr>
2024-11-05 13:21:58 +0200

    Πρώτη δέσμευση: Προσθήκη αρχείου

---
+++ Casting.java
@@ -0,0 +1,39 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		 // Widening casting 
+		
+		int myNumber = 8;
+		
+		double myNumberNew=myNumber;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myNumberNew);
+		
+		
+		char myChar = 'l';
+		
+		double myCharNew = myChar;
+		
+		
+		System.out.println(myChar);
+		System.out.println(myCharNew);
+		
+       // Narrowing Casting 
+		
+		
+		double myDouble = 7.8d;
+		
+		int myInt = (char) myDouble;
+		
+		
+		System.out.println(myDouble);
+		System.out.println(myInt);
+		
+		
+		
+	}
+
+}
